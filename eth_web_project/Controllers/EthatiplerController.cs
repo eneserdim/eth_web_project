0ae0b0409af2d3e5d6883de43c63d6ee130dff8d
Enes Erdim <eneserdim1@gmail.com>
2022-07-05 01:23:30 +0300

    Add project files.

---
+++ eth_web_project/Controllers/EthatiplerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eth_web_project.Controllers
+{
+    public class EthatiplerController : Controller
+    {
+        // GET: Ethatipler
+        public ActionResult anasayfa()
+        {
+            return View();
+        }
+    }
+}
